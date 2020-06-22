88e6878c1376e43b2bd2be163671e08e201a0e63
Andrés Osante <aosantea@ucenfotec.ac.cr>
2020-06-22 17:47:28 -0600

    fixed path require bug

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const connectDB = require('./config/db');
+const path = require('path');
 
 dotenv.config({ path: './config/config.env' });
 connectDB();
